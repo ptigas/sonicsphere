9fed490cf0be5945c080ea02132521cb9a190eab
Daniel Lopez <daniel.lopez999@gmail.com>
2016-09-11 01:20:30 +0100

    Add some settings to node/settings.js
    The github file was empty, for some reason :/. These settings come from
    the project as downloaded from the Dropbox at some point, I think - and
    these settings were used at the Village Underground show.

---
+++ node/settings.js
@@ -0,0 +1,12 @@
+
+// Bluetooth names to whitelist
+exports.whitelist = ["RN42-B1AC", "RN42-B792"];
+
+// OSC port and host to broadcast
+exports.osc = {
+	host : '127.0.0.1',
+	port : 11234
+};
+
+// port of the webgl view (http://localhost:3000)
+exports.view_port = 3000;
